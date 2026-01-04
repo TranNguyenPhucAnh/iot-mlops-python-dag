c13ec7f13de0812dba4b0baacab57ae394c4e1e5
TranNguyenPhucAnh <93979599+TranNguyenPhucAnh@users.noreply.github.com>
2026-01-04 19:50:19 +0700

    Create hello_world.py
    init commit

---
+++ hello_world.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+def print_hello():
+    return "Hello từ hệ thống IoT MLOps của Phúc Anh!"
+
+with DAG(
+    dag_id='hello_world_dag',
+    start_date=datetime(2024, 1, 1),
+    schedule_interval='@daily',
+    catchup=False
+) as dag:
+
+    task_hello = PythonOperator(
+        task_id='print_hello_task',
+        python_callable=print_hello
+    )
